d23ca1e7424f2e411f60850d5419334841aa0b16
César Soto Valero <cesarsotovalero@gmail.com>
2022-09-21 14:43:22 +0200

    Fix warnings in Maven build

---
+++ src/main/java/se/kth/castor/App.java
@@ -0,0 +1,14 @@
+package se.kth.castor;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * Hello world.
+ */
+@Log4j
+public class App {
+
+  public static void main(String[] args) {
+    log.info("Hello World!");
+  }
+}
---
+++ src/test/java/se/kth/castor/AppTest.java
@@ -0,0 +1,36 @@
+package se.kth.castor;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+  /**
+   * Create the test case.
+   *
+   * @param testName name of the test case
+   */
+  public AppTest(String testName) {
+    super(testName);
+  }
+
+  /**
+   * The test suite.
+   *
+   * @return the suite of tests being tested.
+   */
+  public static Test suite() {
+    return new TestSuite(AppTest.class);
+  }
+
+  /**
+   * Rigorous test.
+   */
+  public void testApp() {
+    assertTrue(true);
+  }
+}
