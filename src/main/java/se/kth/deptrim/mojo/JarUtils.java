77afb1b02943ac3c4ccab392ad860c5627bc326e
César Soto Valero <cesarsotovalero@gmail.com>
2022-10-14 19:59:34 +0100

    Add JarUtils

---
+++ src/main/java/se/kth/deptrim/mojo/JarUtils.java
@@ -0,0 +1,77 @@
+package se.kth.deptrim.mojo;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+/**
+ * Utility class for creating jar files.
+ */
+public class JarUtils {
+
+  /**
+   * Create a Jar file from a directory.
+   *
+   * @param directory The directory to be archived.
+   * @param jarName   The name of the jar file.
+   * @throws Exception if an error occurs.
+   */
+  public void createJarFromDirectory(File directory, String jarName) throws Exception {
+    JarOutputStream target = new JarOutputStream(new FileOutputStream(jarName));
+    for (File file : directory.listFiles()) {
+      addFile("", file, target);
+    }
+    target.close();
+  }
+
+  /**
+   * Add a file to the jar file.
+   *
+   * @param parents The parent directories of the file.
+   * @param source  The file to be added.
+   * @param target  The jar file.
+   * @throws Exception if an error occurs.
+   */
+  private void addFile(String parents, File source, JarOutputStream target) throws Exception {
+    BufferedInputStream in = null;
+    try {
+      String name = (parents + source.getName()).replace("\\", "/");
+      if (source.isDirectory()) {
+        if (!name.isEmpty()) {
+          if (!name.endsWith("/")) {
+            name += "/";
+          }
+          JarEntry entry = new JarEntry(name);
+          entry.setTime(source.lastModified());
+          target.putNextEntry(entry);
+          target.closeEntry();
+        }
+        for (File nestedFile : source.listFiles()) {
+          addFile(name, nestedFile, target);
+        }
+        return;
+      }
+      JarEntry entry = new JarEntry(name);
+      entry.setTime(source.lastModified());
+      target.putNextEntry(entry);
+      in = new BufferedInputStream(new FileInputStream(source));
+      byte[] buffer = new byte[1024];
+      while (true) {
+        int count = in.read(buffer);
+        if (count == -1) {
+          break;
+        }
+        target.write(buffer, 0, count);
+      }
+      target.closeEntry();
+    } finally {
+      if (in != null) {
+        in.close();
+      }
+    }
+  }
+
+}
---
+++ src/test/java/se/kth/deptrim/mojo/JarUtilsTest.java
@@ -0,0 +1,42 @@
+package se.kth.deptrim.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.TemporaryFolder;
+
+class JarUtilsTest {
+
+  JarUtils jarUtils;
+  TemporaryFolder temporaryFolder;
+  File jarFile;
+
+  @BeforeEach
+  void setUp() {
+    jarUtils = new JarUtils();
+    jarFile = new File("test.jar");
+    temporaryFolder = new TemporaryFolder(new File("src/test/resources"));
+    try {
+      temporaryFolder.create();
+      temporaryFolder.newFolder("META-INF");
+      temporaryFolder.newFile("META-INF/Manifest.mf");
+    } catch (IOException e) {
+      System.out.println("Error creating temporary folder for testing.");
+    }
+  }
+
+  @Test
+  void ifCreateJarFromDirectoryThenJarShouldExist() throws Exception {
+    jarUtils.createJarFromDirectory(temporaryFolder.getRoot(), "test.jar");
+    Assertions.assertTrue(jarFile.exists() && !jarFile.isDirectory());
+  }
+
+  @AfterEach
+  void tearDown() {
+    temporaryFolder.delete();
+    jarFile.delete();
+  }
+}
