059a56a765f7074369a8bc9e35130583f9ac5571
César Soto Valero <cesarsotovalero@gmail.com>
2022-10-26 09:58:59 +0200

    Improve JarUtilsTest

--- src/test/java/se/kth/deptrim/util/JarUtilsTest.java
+++ src/test/java/se/kth/deptrim/util/JarUtilsTest.java
@@ -2,25 +2,28 @@ package se.kth.deptrim.util;
 
 import java.io.File;
 import java.io.IOException;
-import org.junit.Rule;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.io.TempDir;
 
 class JarUtilsTest {
 
-  @Rule
-  TemporaryFolder temporaryFolder = new TemporaryFolder(new File("src/test/resources"));
+  @TempDir
+  Path tempDir;
+
   File jarFile = new File("test.jar");
 
   @BeforeEach
   void setUp() {
+
     try {
-      temporaryFolder.create();
-      temporaryFolder.newFolder("META-INF");
-      temporaryFolder.newFile("META-INF/Manifest.mf");
+      Path tempFile = Files.createFile(tempDir.resolve("META-INF/Manifest.mf"));
+      Files.writeString(tempFile, "Manifest-Version: 1.0\n"
+          + "Created-By: 1.7.0_06 (Oracle Corporation)");
     } catch (IOException e) {
       System.out.println("Error creating temporary folder for testing.");
     }
@@ -28,13 +31,12 @@ class JarUtilsTest {
 
   @Test
   void ifCreateJarFromDirectoryThenJarShouldExist() throws Exception {
-    JarUtils.createJarFromDirectory(temporaryFolder.getRoot(), jarFile);
+    JarUtils.createJarFromDirectory(tempDir.toFile(), jarFile);
     Assertions.assertTrue(jarFile.exists() && !jarFile.isDirectory());
   }
 
   @AfterEach
   void tearDown() {
-    temporaryFolder.delete();
     jarFile.delete();
   }
 }
