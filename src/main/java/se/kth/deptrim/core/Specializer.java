6c50205c08b942354038f0b921a575dcf13b0a7f
César Soto Valero <cesarsotovalero@gmail.com>
2023-01-31 12:13:53 +0100

    Keep logs when all types are used

--- src/main/java/se/kth/deptrim/core/Specializer.java
+++ src/main/java/se/kth/deptrim/core/Specializer.java
@@ -70,7 +70,7 @@ public class Specializer {
   /**
    * Trim the unused classes from the dependencies specified by the user based on the usage analysis results.
    *
-   * @param analysis         The dependency usage analysis results
+   * @param analysis               The dependency usage analysis results
    * @param specializeDependencies The dependencies to be trimmed, if empty then trims all the dependencies
    */
   @SneakyThrows
@@ -96,7 +96,8 @@ public class Specializer {
             unusedTypes.removeAll(types.getUsedTypes());
             String dependencyDirName = dependency.getFile().getName().substring(0, dependency.getFile().getName().length() - 4);
             File srcDir = dependencyManager.getBuildDirectory().resolve(DIRECTORY_TO_EXTRACT_DEPENDENCIES + File.separator + dependencyDirName).toFile();
-            File destDir = dependencyManager.getBuildDirectory().resolve(DIRECTORY_TO_LOCATE_THE_DEBLOATED_DEPENDENCIES + File.separator + dependencyDirName).toFile();
+            File destDir = dependencyManager.getBuildDirectory().resolve(DIRECTORY_TO_LOCATE_THE_DEBLOATED_DEPENDENCIES + File.separator + dependencyDirName)
+                .toFile();
 
             // Copy all files from srcDir to destDir
             try {
@@ -106,58 +107,57 @@ public class Specializer {
             }
 
             // Remove files in destDir.
-            if (!unusedTypes.isEmpty()) {
-              log.info("Specializing dependency " + dependencyCoordinates + ", removing " + unusedTypes.size() + "/" + types.getAllTypes().size() + " unused types.");
-              for (ClassName className : unusedTypes) {
-                String fileName = className.toString().replace(".", File.separator) + ".class";
-                File file = new File(destDir.getAbsolutePath() + File.separator + fileName);
-                try {
-                  Files.delete(file.toPath());
-                } catch (IOException e) {
-                  log.error("Error deleting file " + file.getPath());
-                }
-              }
-
-              // Delete all empty directories in destDir.
-              se.kth.deptrim.util.FileUtils fileUtils = new se.kth.deptrim.util.FileUtils();
-              fileUtils.deleteEmptyDirectories(destDir);
-
-              // Create a new jar file with the debloated classes and move it to libs-specialized.
-              Path libSpecializedPath = Paths.get("libs-specialized");
-              String jarName = destDir.getName() + ".jar";
-              File jarFile = libSpecializedPath.resolve(jarName).toFile();
-              try {
-                Files.createDirectories(libSpecializedPath); // create libs-deptrim directory if it does not exist
-                se.kth.deptrim.util.JarUtils.createJarFromDirectory(destDir, jarFile);
-              } catch (Exception e) {
-                log.error("Error creating specialized jar for " + destDir.getName());
-              }
-
-              // Deploy specialized jars to the local Maven repository.
-              try {
-                String mavenDeployCommand = "mvn deploy:deploy-file -Durl="
-                    + mavenLocalRepoUrl
-                    + " -Dpackaging=jar"
-                    + " -Dfile=" + jarFile.getAbsolutePath()
-                    + " -DgroupId=" + GROUP_ID_OF_SPECIALIZED_JAR
-                    + " -DartifactId=" + dependency.getDependencyId()
-                    + " -Dversion=" + dependency.getVersion();
-                log.info(mavenDeployCommand);
-                MavenInvoker.runCommand(mavenDeployCommand, null);
-                // If successfully deployed
-                SpecializedDependency specializedDependency = new SpecializedDependency(
-                    dependency.getGroupId(),
-                    dependency.getDependencyId(),
-                    dependency.getVersion(),
-                    GROUP_ID_OF_SPECIALIZED_JAR
-                );
-                deployedSpecializedDependencies.add(specializedDependency);
-              } catch (IOException | InterruptedException e) {
-                log.error("Error installing the specialized dependency JAR in the local repository.");
-                Thread.currentThread().interrupt();
-              }
-            } else {
+            log.info("Specializing dependency " + dependencyCoordinates + ", removing " + unusedTypes.size() + "/" + types.getAllTypes().size() + " unused types.");
+            if (unusedTypes.isEmpty()){
               log.info("Skipping specializing dependency " + dependencyCoordinates + " because all its types are used.");
+            }
+            for (ClassName className : unusedTypes) {
+              String fileName = className.toString().replace(".", File.separator) + ".class";
+              File file = new File(destDir.getAbsolutePath() + File.separator + fileName);
+              try {
+                Files.delete(file.toPath());
+              } catch (IOException e) {
+                log.error("Error deleting file " + file.getPath());
+              }
+            }
+
+            // Delete all empty directories in destDir.
+            se.kth.deptrim.util.FileUtils fileUtils = new se.kth.deptrim.util.FileUtils();
+            fileUtils.deleteEmptyDirectories(destDir);
+
+            // Create a new jar file with the debloated classes and move it to libs-specialized.
+            Path libSpecializedPath = Paths.get("libs-specialized");
+            String jarName = destDir.getName() + ".jar";
+            File jarFile = libSpecializedPath.resolve(jarName).toFile();
+            try {
+              Files.createDirectories(libSpecializedPath); // create libs-deptrim directory if it does not exist
+              se.kth.deptrim.util.JarUtils.createJarFromDirectory(destDir, jarFile);
+            } catch (Exception e) {
+              log.error("Error creating specialized jar for " + destDir.getName());
+            }
+
+            // Deploy specialized jars to the local Maven repository.
+            try {
+              String mavenDeployCommand = "mvn deploy:deploy-file -Durl="
+                  + mavenLocalRepoUrl
+                  + " -Dpackaging=jar"
+                  + " -Dfile=" + jarFile.getAbsolutePath()
+                  + " -DgroupId=" + GROUP_ID_OF_SPECIALIZED_JAR
+                  + " -DartifactId=" + dependency.getDependencyId()
+                  + " -Dversion=" + dependency.getVersion();
+              log.info(mavenDeployCommand);
+              MavenInvoker.runCommand(mavenDeployCommand, null);
+              // If successfully deployed
+              SpecializedDependency specializedDependency = new SpecializedDependency(
+                  dependency.getGroupId(),
+                  dependency.getDependencyId(),
+                  dependency.getVersion(),
+                  GROUP_ID_OF_SPECIALIZED_JAR
+              );
+              deployedSpecializedDependencies.add(specializedDependency);
+            } catch (IOException | InterruptedException e) {
+              log.error("Error installing the specialized dependency JAR in the local repository.");
+              Thread.currentThread().interrupt();
             }
           }
         });
